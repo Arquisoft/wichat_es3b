e2a2fc6cdcf347a1e56597345235dba6a27bd9fe
uo287935 <uo287935@uniovi.es>
2025-03-29 12:26:54 +0100

    Quitar prueba de mongo en los test

--- wikiquestionservice/wikiQuestion-service.test.js
+++ wikiquestionservice/wikiQuestion-service.test.js
@@ -1,19 +1,11 @@
-const request = require('supertest');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-
-let mongoServer;
 let app;
 
 beforeAll(async () => {
-    mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri();
-    process.env.MONGODB_URI = mongoUri;
     app = require('./wikiQuestion-service'); // Ajusta la ruta según tu estructura
 });
 
 afterAll(async () => {
     app.close();
-    await mongoServer.stop();
 });
 
 describe('WikiQuestion Service', () => {
