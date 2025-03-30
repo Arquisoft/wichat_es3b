6dcc37fb861c64be08e576a44a4befc2d45b3aca
alejandroVG2305 <alejandroVG235@gmail.com>
2025-03-30 13:00:57 +0200

    Incorporado cuadro de diálogo de reglas y arreglado el footer.

---
+++ webapp/src/components/infoDialog/InfoDialog.js
@@ -0,0 +1,17 @@
+import React from "react";
+import BaseButton from "../button/BaseButton";
+import "./InfoDialog.css";
+
+const InfoDialog = ({ title, content, onClose }) => {
+  return (
+    <div className="dialogContainer">
+      <h1>{title}</h1>
+      <div className="dialogMessageContainer">{content}</div>
+      <div className="dialogButtonContainer">
+        <BaseButton text="Entendido" onClick={onClose}></BaseButton>
+      </div>
+    </div>
+  );
+};
+
+export default InfoDialog;
--- webapp/src/pages/game/Game.js
+++ webapp/src/pages/game/Game.js
@@ -5,14 +5,22 @@ import Footer from "../../components/Footer";
 import HintButton from "../../components/hintButton/HintButton";
 import BaseButton from "../../components/button/BaseButton";
 import ChatBox from "../../components/chatBox/ChatBox";
-import { LinearProgress, Box, Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
+import InfoDialog from "../../components/infoDialog/InfoDialog";
+import {
+  LinearProgress,
+  Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 import { useTranslation } from "react-i18next";
 
 const TOTAL_TIME = 40;
 const USERNAME = "jugador1";
-
 
 const Game = () => {
   const { i18n } = useTranslation();
@@ -35,9 +43,7 @@ const Game = () => {
   const [showSummary, setShowSummary] = useState(false);
   const timerRef = useRef(null);
   const [totalTimeUsed, setTotalTimeUsed] = useState(0);
-
-
-
+  const [showRules, setShowRules] = useState(false);
 
   const URL = "http://localhost:8004/";
   const STATS_URL = "http://localhost:8005/savestats";
@@ -63,14 +69,14 @@ const Game = () => {
     fetchQuestions();
   }, [fetchQuestions]);
 
-  useEffect( () => {
-    if (timeLeft <= 0 ) {// Si el contador de tiempo llega a 0, se cuenta la pregunta actual como incorrecta
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      // Si el contador de tiempo llega a 0, se cuenta la pregunta actual como incorrecta
       setIncorrectAnswers((prev) => prev + 1);
       const timeUsed = TOTAL_TIME - timeLeft; // Tiempo usado en esta pregunta
       setTotalTimeUsed((prev) => prev + timeUsed);
       return;
     }
-
 
     timerRef.current = setInterval(() => {
       setTimeLeft((prev) => Math.max(prev - 1, 0));
@@ -105,7 +111,7 @@ const Game = () => {
     }
   };
 
-  const handleNextQuestion =  () => {
+  const handleNextQuestion = () => {
     setQuestionNumber((prevNumber) => {
       const newNumber = prevNumber + 1;
       setCurrentQuestion(questions[newNumber]);
@@ -127,7 +133,6 @@ const Game = () => {
       setIsCorrect(true);
       setScore((prevScore) => prevScore + 10);
       setCorrectAnswers((prev) => prev + 1);
-
     } else {
       setIsCorrect(false);
       setIncorrectAnswers((prev) => prev + 1);
@@ -144,152 +149,183 @@ const Game = () => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
     return `${minutes.toString().padStart(2, "0")}:${seconds
-        .toString()
-        .padStart(2, "0")}`;
-
+      .toString()
+      .padStart(2, "0")}`;
   };
 
   const toggleChatBox = () => {
     setIsChatBoxVisible(!isChatBoxVisible);
-
   };
-
-
 
   if (isLoading) {
     return <div>Cargando...</div>;
   }
 
   return (
-      <div>
-        <Nav />
-        <main>
-          <div className="upperSection">
-            <div className="hintButton">
-              <HintButton
+    <div className="gameContainer">
+      <Nav />
+      <main className={showRules ? "blurred" : ""}>
+        <div className="upperSection">
+          <div className="hintButton">
+            <HintButton
+              text={
+                isChatBoxVisible ? "Ocultar pistas" : "¿Necesitas una pista?"
+              }
+              onClick={toggleChatBox}
+            />
+          </div>
+          <div className="question">
+            <div className="questionNumber">
+              <h2>{`Pregunta ${questionNumber + 1}/25`}</h2>
+              <ArrowForwardIcon
+                titleAccess="Siguiente pregunta"
+                fontSize="1.5em"
+                id="nextArrow"
+                onClick={isAnswered ? handleNextQuestion : null}
+                style={{
+                  color: isAnswered ? "white" : "gray",
+                  cursor: isAnswered ? "pointer" : "not-allowed",
+                }}
+              ></ArrowForwardIcon>
+            </div>
 
-                  text={isChatBoxVisible ? "Ocultar pistas" : "¿Necesitas una pista?"}
-                  onClick={toggleChatBox}
-
-               
-
+            <h1>{currentQuestion.pregunta[currentLanguage]}</h1>
+          </div>
+          <div className="rightUpperSection">
+            <div className="pointsAndRules">
+              <div>
+                <span>Puntuación: </span> <span className="score">{score}</span>
+              </div>
+              <BaseButton
+                text={"Reglas"}
+                buttonType="buttonSecondary"
+                onClick={() => setShowRules(true)}
               />
             </div>
-            <div className="question">
-              <div className="questionNumber">
-                <h2>{`Pregunta ${questionNumber + 1}/25`}</h2>
-                <ArrowForwardIcon
-                    titleAccess="Siguiente pregunta"
-                    fontSize="1.5em"
-                    id="nextArrow"
-                    onClick={isAnswered ? handleNextQuestion : null}
-                    style={{
-                      color: isAnswered ? "white" : "gray",
-                      cursor: isAnswered ? "pointer" : "not-allowed",
-                    }}
-                ></ArrowForwardIcon>
-              </div>
-
-              <h1>{currentQuestion.pregunta[currentLanguage]}</h1>
-
-
-            </div>
-            <div className="rightUpperSection">
-              <div className="pointsAndRules">
-                <div>
-                  <span>Puntuación: </span> <span className="score">{score}</span>
-                </div>
-                <BaseButton text={"Reglas"} buttonType="buttonSecondary" />
-              </div>
-            </div>
           </div>
-          <div className="midSection">
-
-
-
-            {currentQuestion.img && (
-                <img src={currentQuestion.img[0]} alt="imagen pregunta"></img>
-            )}
-            <div className="answerPanel">
-              {currentQuestion.respuestas &&
-
-                  currentQuestion.respuestas[currentLanguage].map((respuesta, index) => (
-
-
-                      <BaseButton
-                          key={index}
-                          text={respuesta}
-                          onClick={() => handleAnswerClick(respuesta)}
-                          buttonType={
-                            isAnswered
-
-                                ? respuesta === currentQuestion.respuestaCorrecta[currentLanguage]
-
-
-                                    ? "buttonCorrect"
-                                    : selectedAnswer === respuesta
-                                        ? "buttonIncorrect"
-                                        : "buttonPrimary"
-                                : "buttonPrimary"
-                          }
-                          disabled={isAnswered}
-                      ></BaseButton>
-                  ))}
-            </div>
+        </div>
+        <div className="midSection">
+          {currentQuestion.img && (
+            <img src={currentQuestion.img[0]} alt="imagen pregunta"></img>
+          )}
+          <div className="answerPanel">
+            {currentQuestion.respuestas &&
+              currentQuestion.respuestas[currentLanguage].map(
+                (respuesta, index) => (
+                  <BaseButton
+                    key={index}
+                    text={respuesta}
+                    onClick={() => handleAnswerClick(respuesta)}
+                    buttonType={
+                      isAnswered
+                        ? respuesta ===
+                          currentQuestion.respuestaCorrecta[currentLanguage]
+                          ? "buttonCorrect"
+                          : selectedAnswer === respuesta
+                          ? "buttonIncorrect"
+                          : "buttonPrimary"
+                        : "buttonPrimary"
+                    }
+                    disabled={isAnswered}
+                  ></BaseButton>
+                )
+              )}
           </div>
-          <div className="lowerSection">
-            <Box
-                display="flex"
-                alignItems="center"
-                width="50%"
-                margin="auto"
-                gap={2}
-            >
-              <span>Tiempo</span>
-              <Box width="100%" position="relative">
-                <LinearProgress
-                    id="progressBar"
-                    variant="determinate"
-                    value={progress}
-                ></LinearProgress>
-              </Box>
-              <span>{formatTime(timeLeft)}</span>
+        </div>
+        <div className="lowerSection">
+          <Box
+            display="flex"
+            alignItems="center"
+            width="50%"
+            margin="auto"
+            gap={2}
+          >
+            <span>Tiempo</span>
+            <Box width="100%" position="relative">
+              <LinearProgress
+                id="progressBar"
+                variant="determinate"
+                value={progress}
+              ></LinearProgress>
             </Box>
-          </div>
-          <div></div>
+            <span>{formatTime(timeLeft)}</span>
+          </Box>
+        </div>
+        <div></div>
 
-          <div className={`chatBoxContainer ${isChatBoxVisible ? 'visible' : 'hidden'}`}>
-            <ChatBox  question={{
+        <div
+          className={`chatBoxContainer ${
+            isChatBoxVisible ? "visible" : "hidden"
+          }`}
+        >
+          <ChatBox
+            question={{
               pregunta: currentQuestion.pregunta.es,
               respuestaCorrecta: currentQuestion.respuestaCorrecta.es,
               respuestas: currentQuestion.respuestas.es,
               descripcion: currentQuestion.descripcion,
-              img: currentQuestion.img
-            }}  language="es" isVisible={true} />
+              img: currentQuestion.img,
+            }}
+            language="es"
+            isVisible={true}
+          />
+        </div>
+      </main>
+      {showRules && (
+        <div className="overlay">
+          <div className="dialogGameRulesContainer">
+            <InfoDialog
+              title={"Reglas del juego"}
+              content={
+                <ol>
+                  <li>Observa la imagen.</li>
+                  <li>
+                    Responde en el menor tiempo posible, dentro de los límites
+                    ofrecidos.
+                  </li>
+                  <li>
+                    Usa las pistas generadas por nuestra IA si lo necesitas.
+                  </li>
+                </ol>
+              }
+              onClose={() => setShowRules(false)}
+            />
           </div>
-        </main>
-        <Footer />
-
-        <Dialog open={showSummary} onClose={() => setShowSummary(false)}>
-          <DialogTitle>Resumen de la partida</DialogTitle>
-          <DialogContent>
-            <p>Respuestas correctas: {correctAnswers}</p>
-            <p>Respuestas incorrectas: {incorrectAnswers}</p>
-            <p>Ratio de aciertos: {(correctAnswers / (correctAnswers + incorrectAnswers || 1)).toFixed(2)}</p>
-            <p>Tiempo promedio por pregunta: {(totalTimeUsed / (correctAnswers + incorrectAnswers || 1)).toFixed(2)}s</p>
-            <p>Puntuación máxima: {score}</p>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={async () =>{
+        </div>
+      )}
+      <Footer />
+      <Dialog open={showSummary} onClose={() => setShowSummary(false)}>
+        <DialogTitle>Resumen de la partida</DialogTitle>
+        <DialogContent>
+          <p>Respuestas correctas: {correctAnswers}</p>
+          <p>Respuestas incorrectas: {incorrectAnswers}</p>
+          <p>
+            Ratio de aciertos:{" "}
+            {(
+              correctAnswers / (correctAnswers + incorrectAnswers || 1)
+            ).toFixed(2)}
+          </p>
+          <p>
+            Tiempo promedio por pregunta:{" "}
+            {(totalTimeUsed / (correctAnswers + incorrectAnswers || 1)).toFixed(
+              2
+            )}
+            s
+          </p>
+          <p>Puntuación máxima: {score}</p>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={async () => {
               setShowSummary(false);
               window.location.reload();
-            }}>
-              Cerrar
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-      </div>
+            }}
+          >
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 export default Game;
