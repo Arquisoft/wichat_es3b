2b78bcb99ea88f92db0532d35098694667fc3ca3
UO291047 <145019884+UO291047@users.noreply.github.com>
2025-04-03 15:06:10 +0200

    Primera versión del API Service funcional (#157)
    * Cambios para la implementación de la API. Versión no funcional.
    
    * Ni Adonalsium tardó tanto en recomponerse. Al fin funciona
    
    * Documentación de la API actualizada. Funcionalidad de obtención de preguntas desde la API completada.
    
    * Cambios para facilitar el merge con dev
    
    * Versión preeliminar, utilizando localhost funciona
    
    * Actualizado el sistema, con API_URI en el env al endpoint que queramos ya funciona.
    
    * API service creado. Versión funcional
    
    ---------
    
    Co-authored-by: uo294914 <uo294914@uniovi.es>

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -2,16 +2,12 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const promBundle = require("express-prom-bundle");
-//libraries required for OpenAPI-Swagger
-const swaggerUi = require("swagger-ui-express");
-const fs = require("fs");
-const YAML = require("yaml");
 
 const app = express();
 const port = 8000;
 
 const wikiQuestionServiceUrl =
-  process.env.WIKIQUESTION_SERVICE_URL || "http://localhost:8004";
+  process.env.QUESTION_SERVICE_URL || "http://localhost:8004";
 const llmServiceUrl = process.env.LLM_SERVICE_URL || "http://localhost:8003";
 const authServiceUrl = process.env.AUTH_SERVICE_URL || "http://localhost:8002";
 const userServiceUrl = process.env.USER_SERVICE_URL || "http://localhost:8001";
@@ -172,21 +168,17 @@ app.get("/ratios-per-month/:username", async (req, res) => {
   }
 });
 
-// Read the OpenAPI YAML file synchronously
-openapiPath = "./openapi.yaml";
-if (fs.existsSync(openapiPath)) {
-  const file = fs.readFileSync(openapiPath, "utf8");
-
-  // Parse the YAML content into a JavaScript object representing the Swagger document
-  const swaggerDocument = YAML.parse(file);
-
-  // Serve the Swagger UI documentation at the '/api-doc' endpoint
-  // This middleware serves the Swagger UI files and sets up the Swagger UI page
-  // It takes the parsed Swagger document as input
-  app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-} else {
-  console.log("Not configuring OpenAPI. Configuration file not present.");
-}
+// Question endpoints
+app.get('/questions/:n', async (req, res) => {
+    try {
+        const { n } = req.params;
+        console.log(`${wikiQuestionServiceUrl}/questions`);
+        const response = await axios.get(`${wikiQuestionServiceUrl}/questions?n=${n}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(error.response ? error.response.status : 500).json({ error: error.message });
+    }
+});
 
 // Start the gateway service
 const server = app.listen(port, () => {
