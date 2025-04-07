4449ad08ad4fa5009955de1ed7060af1678c07a5
uo287935 <uo287935@uniovi.es>
2025-04-07 16:53:06 +0200

    Nav test añadido

--- webapp/src/components/Footer.test.js
+++ webapp/src/components/Footer.test.js
@@ -1,15 +1,12 @@
 import {render, screen} from "@testing-library/react";
 import {I18nextProvider} from "react-i18next";
 import i18n from "../i18n";
-import {MemoryRouter} from "react-router-dom";
 import Footer from "./Footer";
 
 const renderFooter = () => {
     render(
         <I18nextProvider i18n={i18n}>
-            <MemoryRouter>
                 <Footer handleToggleView={jest.fn()} />
-            </MemoryRouter>
         </I18nextProvider>
     );
 };
@@ -18,6 +15,6 @@ describe("Footer component", () => {
     test("Renderiza correctamente el footer", () => {
         renderFooter();
 
-        expect(screen.getByText(i18n.t("footer_text")).toBeInTheDocument());
+        expect(screen.getByText(i18n.t("footer_text"))).toBeInTheDocument();
     });
 });
