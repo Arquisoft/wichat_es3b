7aa007899e5fca7bd3bc9ca20b6cd636ccc82b57
alejandroVG2305 <alejandroVG235@gmail.com>
2025-05-02 21:28:24 +0200

    Implementados test de Api Key.

--- gatling-tests/src/test/scala/es/uniovi/arquisoft/wichat/loadtests/ApiKey.scala
+++ gatling-tests/src/test/scala/es/uniovi/arquisoft/wichat/loadtests/ApiKey.scala
@@ -1,12 +1,16 @@
 package es.uniovi.arquisoft.wichat.loadtests
 
 import scala.concurrent.duration._
+import java.util.UUID
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
 
 class ApiKey extends Simulation {
+
+  val customFeeder = Iterator.continually(
+    Map("email" -> (s"user_${UUID.randomUUID().toString.take(8)}@example.com"))
+  )
 
   private val httpProtocol = http
     .baseUrl("http://localhost:8000")
@@ -15,39 +19,40 @@ class ApiKey extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Gecko/20100101 Firefox/138.0")
-  
+
   private val headers_0 = Map(
-  		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-  		"If-None-Match" -> """"c6e28276a450aa3cc3304adb76859bb173f5f4f7"""",
-  		"Priority" -> "u=0, i",
-  		"Upgrade-Insecure-Requests" -> "1"
+    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "If-None-Match" -> """"c6e28276a450aa3cc3304adb76859bb173f5f4f7"""",
+    "Priority" -> "u=0, i",
+    "Upgrade-Insecure-Requests" -> "1"
   )
-  
+
   private val headers_1 = Map("If-None-Match" -> """"a09770fdc1485eb7ef9f7f81d2efc0db2af89a3a"""")
-  
+
   private val headers_2 = Map(
-  		"Accept" -> "text/css,*/*;q=0.1",
-  		"If-None-Match" -> """"5df8c96bebc6b6e3be913ab40a53a46c47154111"""",
-  		"Priority" -> "u=2"
+    "Accept" -> "text/css,*/*;q=0.1",
+    "If-None-Match" -> """"5df8c96bebc6b6e3be913ab40a53a46c47154111"""",
+    "Priority" -> "u=2"
   )
-  
+
   private val headers_3 = Map(
-  		"Access-Control-Request-Headers" -> "content-type",
-  		"Access-Control-Request-Method" -> "POST",
-  		"Origin" -> "http://localhost:3000",
-  		"Priority" -> "u=4"
+    "Access-Control-Request-Headers" -> "content-type",
+    "Access-Control-Request-Method" -> "POST",
+    "Origin" -> "http://localhost:3000",
+    "Priority" -> "u=4"
   )
-  
+
   private val headers_4 = Map(
-  		"Accept" -> "application/json, text/plain, */*",
-  		"Content-Type" -> "application/json",
-  		"Origin" -> "http://localhost:3000",
-  		"Priority" -> "u=0"
+    "Accept" -> "application/json, text/plain, */*",
+    "Content-Type" -> "application/json",
+    "Origin" -> "http://localhost:3000",
+    "Priority" -> "u=0"
   )
-  
+
   private val uri1 = "localhost"
 
   private val scn = scenario("ApiKey")
+    .feed(customFeeder)
     .exec(
       http("request_0")
         .get("http://" + uri1 + ":3000/")
@@ -68,9 +73,9 @@ class ApiKey extends Simulation {
           http("request_4")
             .post("/generate-apikey")
             .headers(headers_4)
-            .body(RawFileBody("es/uniovi/arquisoft/wichat/loadtests/apikey/0004_request.json"))
+            .body(StringBody("""{"email": "${email}"}""")).asJson
         )
     )
 
-	setUp(scn.inject(constantUsersPerSec(5).during(15))).protocols(httpProtocol)
+  setUp(scn.inject(constantUsersPerSec(5).during(15))).protocols(httpProtocol)
 }
