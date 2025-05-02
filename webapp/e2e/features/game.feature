460868c1b54edbd08663f21f677bca6653edbd8a
nalleta <UO294914@uniovi.es>
2025-05-02 20:08:20 +0200

    Añadidos tests para iniciar partida y para jugar 2 partidas

--- webapp/e2e/features/game.feature
+++ webapp/e2e/features/game.feature
@@ -1,6 +1,16 @@
 Feature: Game Feature
 
+  Scenario: The user is able to start playing a game
+    Given A logged user
+    When I configure and start a new game
+    Then The game page should be shown
+
   Scenario: The user is able to play a game
     Given A logged user
     When I play a game
     Then The results page should be shown
+
+  Scenario: The user is able to restart a game
+    Given A logged user
+    When I play a game and start another
+    Then A new question should be shown
--- webapp/e2e/steps/game.steps.js
+++ webapp/e2e/steps/game.steps.js
@@ -47,6 +47,8 @@ defineFeature(feature, test => {
             if (!url.includes('/home')) {
                 throw new Error('Login failed or redirection did not occur');
             }
+            // Esperamos a que carguen todas las preguntas.
+            await page.waitForTimeout(10000);
         } catch (error) {
             console.error('Error during login process: ', error);
             await browser.close();
@@ -54,27 +56,38 @@ defineFeature(feature, test => {
         }
     });
 
-    test('The user is able to play a game', ({ given, when, then }) => {
+    beforeEach(async () => {
+        await page.goto("http://localhost:3000/home", {
+            waitUntil: "networkidle0",
+        });
+    });
 
+    test('The user is able to start playing a game', ({ given, when, then }) => {
         given('A logged user', async () => {
             // Ensure the game page is loaded
             await page.waitForSelector('button', { visible: true, timeout: 5000 });
         });
 
+        when('I configure and start a new game', async () => {
+            await startDefaultGame();
+        }
+        );
+
+        then('The game page should be shown', async () => {
+            await page.waitForSelector('#answer-1', { visible: true, timeout: 500000 });
+        });
+    },600000000);
+
+    test('The user is able to play a game', ({ given, when, then }) => {
+
+        given('A logged user', async () => {
+            await page.waitForSelector('button', { visible: true, timeout: 5000 });
+        });
+
         when('I play a game', async () => {
             try{
-                // Esperando por preguntas
-                //await page.waitForTimeout(10000);
-                await expect(page).toClick('a', { text: 'Jugar' });
-                await page.waitForSelector('#numPreguntas', { visible: true, timeout: 5000 });
-                await page.select('#numPreguntas', '10');
-                await expect(page).toClick('button', { text: 'Jugar' });
-                for (let i = 1; i < 10; i++) {
-                    await page.waitForSelector('#answer-1', { visible: true, timeout: 500000 });
-                    await expect(page).toClick('#answer-1', {});
-                    await page.waitForSelector('#nextArrow', { visible: true, timeout: 5000 });
-                    await expect(page).toClick('#nextArrow', {});
-                }
+                await startDefaultGame();
+                await playA10QGame();
                 await page.waitForSelector('#answer-1', { visible: true, timeout: 5000 });
                 await expect(page).toClick('#answer-1', {});
             } catch (error) {
@@ -95,12 +108,48 @@ defineFeature(feature, test => {
 
         then('The results page should be shown', async () => {
             await page.waitForSelector('h1', { text: "Resumen de la partida", timeout: 5000 });
-
-            // Lo anterior espera a que el elemento aparezca, no debería hacer falta compararlo, ya que esto no coge el h1 que debería.
-            // const finalScoreText = await page.$eval('h1', el => el.textContent.trim());
-            // expect(finalScoreText).toBe('Resumen de la partida');
         });
     },600000000);
+
+    test('The user is able to restart a game', ({ given, when, then }) => {
+        given('A logged user', async () => {
+            // Ensure the game page is loaded
+            await page.waitForSelector('button', { visible: true, timeout: 5000 });
+        });
+
+        when('I play a game and start another', async () => {
+
+            await startDefaultGame();
+            await playA10QGame();
+            await expect(page).toClick('button', {text: "Entendido"});
+            await page.waitForSelector('#numPreguntas', { visible: true, timeout: 5000 });
+            await page.select('#numPreguntas', '10');
+            await expect(page).toClick('button', { text: 'Jugar' });
+        }
+        );
+
+        then('A new question should be shown', async () => {
+            await page.waitForSelector('#answer-1', { visible: true, timeout: 500000 });
+        });
+    },600000000);
+
+    async function startDefaultGame() {
+        await expect(page).toClick('a', { text: 'Jugar' });
+        await page.waitForSelector('#numPreguntas', { visible: true, timeout: 5000 });
+        await page.select('#numPreguntas', '10');
+        await expect(page).toClick('button', { text: 'Jugar' });
+    }
+
+    async function playA10QGame() {
+        for (let i = 1; i < 10; i++) {
+            await page.waitForSelector('#answer-1', { visible: true, timeout: 500000 });
+            await expect(page).toClick('#answer-1', {});
+            await page.waitForSelector('#nextArrow', { visible: true, timeout: 5000 });
+            await expect(page).toClick('#nextArrow', {});
+        }
+        await page.waitForSelector('#answer-1', { visible: true, timeout: 5000 });
+        await expect(page).toClick('#answer-1', {});
+    }
 
     afterAll(async () => {
         await browser.close();
